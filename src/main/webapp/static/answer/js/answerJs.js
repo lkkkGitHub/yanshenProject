eaea28b078ea2f64adc56e678551c6ce0aa05d48
lkkkGitHub <963087276@qq.com>
2018-11-12 21:19:52 +0800

    存在的bug：通过js获取选择的单选框的value值，只能清除缓存之后才能生效，以及后台的选项记录optionid

--- src/main/webapp/static/answer/js/answerJs.js
+++ src/main/webapp/static/answer/js/answerJs.js
@@ -4,7 +4,7 @@ function intiTopicInfo(sequence, sequenceNext, optionId) {
     $.ajax({
         url: "/topic/getTopicPagination",
         type: "get",
-        async: true,
+        async: false,
         //(默认: true) 默认设置下，所有请求均为异步请求。如果需要发送同步请求，请将此选项设置为 false。注意，
         // 同步请求将锁住浏览器，用户其它操作必须等待请求完成才可以执行。
         contentType: "application/x-www-form-urlencoded",
@@ -26,7 +26,9 @@ function intiTopicInfo(sequence, sequenceNext, optionId) {
                 str += "<input data-toggle=\"radio\" name=\"option\" class=\"option\" value=\""+data.optionList[i].optionId+"\" id=\"optionInput" + i + "\" type=\"radio\">";
                 str += "<pre>"+data.optionList[i].optionId+"   "+data.optionList[i].optionComment+"</pre></label></a>";
                 if (data.optionId != null) {
-                    checkedAndNoChecked(i);
+                    if (data.optionId == data.optionList[i].optionId) {
+                        checkedAndNoChecked(i);
+                    }
                 }
             }
             $(".subject-content").html(str);
@@ -36,6 +38,7 @@ function intiTopicInfo(sequence, sequenceNext, optionId) {
 
 //单选框的选中
 function checkedAndNoChecked(id) {
+    event.preventDefault();
     var chackA = document.getElementById("optionA" + id);
     var chack = document.getElementById("option" + id);
     var nocheckedAll = document.getElementsByClassName("option");
@@ -46,7 +49,7 @@ function checkedAndNoChecked(id) {
     }
     chackA.classList.add("selected");
     chack.classList.add("checked");
-    $('#optionInput' + id).attr('checked', true);
+    $('#optionInput'+id).attr('checked', true);
 }
 
 //下导航条的题目num刷新   以及想去的下一题序号
@@ -56,16 +59,23 @@ function answeringNum(sequenceNext, topicNum) {
     var sequence = null;
     //当前作答的题目选项id
     var optionId = $("input[name='option']:checked").val();
-    if (optionId == null) {
-        optionId = -1;
-    }
     //获取当前的题目顺序号
     for (var i = 0; i < topicNum; i++) {
         if ($('#topicIndex' + i).is('.answering-num')) {
             sequence = i;
+            break;
         }
     }
-    alert(optionId);
+    var topicIndex = document.getElementById("topicIndex"+sequence);
+    topicIndex.classList.remove("answering-num");
+    if (optionId == null) {
+        optionId = -1;
+    } else {
+        topicIndex.classList.add("answer-done");
+    }
+    // alert("sequenceNext" + sequenceNext);
+    // alert("sequenceNext" + optionId);
+    document.getElementById("topicIndex"+sequenceNext).classList.add("answering-num");
     //传入当前题目顺序号，以及题目的选择
     intiTopicInfo(sequence, sequenceNext, optionId);
 }
