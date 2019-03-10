f25431f631df8747a93b919d39d9c9c145763087
leike <963087276@qq.com>
2019-03-10 20:03:39 +0800

    webSocket 配置文件版本

---
+++ src/main/java/com/tools/utils/websocket/GetHttpSessionConfigurator.java
@@ -0,0 +1,19 @@
+package com.tools.utils.websocket;
+
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+
+/**
+ * @author leike
+ * @date 2019/3/8 13:45
+ */
+public class GetHttpSessionConfigurator extends ServerEndpointConfig.Configurator {
+    @Override
+    public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
+        HttpSession httpSession = (HttpSession) request.getHttpSession();
+        sec.getUserProperties().put(HttpSession.class.getName(), httpSession);
+    }
+}
